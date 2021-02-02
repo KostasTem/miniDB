e255670c1bbc89c6183905c5f32babfd68cf03f7
KostasTem <kostas@DESKTOP-9O3MLP7.localdomain>
2021-02-02 17:27:21 +0200

    Updated query handling functions

--- server.py
+++ server.py
@@ -8,24 +8,34 @@ HOST = '127.0.0.1'
 PORT = 65432  
 
 def getTable(inp):
-    t = inp.split("from")[1]
-    t = t.split(" ")[1]
-    return t.strip()
+    t = inp[inp.index("from")+1]
+    return t
 def getCols(inp):
-    cols = inp.split("from")[0]
-    cols = cols.split("select")[1]
-    if "," in cols:
-        cols = cols.split(",")
-    if type(cols) is list:
-        for i in range(len(cols)):
-            cols[i] = cols[i].strip()
-        return cols
-    else:
-        cols = cols.strip()
-        return [cols]
+    st = inp.index("select")
+    end = inp.index("from")
+    cols = list()
+    if end-(st+1)==1:
+        if "," in inp[st+1]:
+            tmp = inp[st+1].split(",")
+            for el in tmp:
+                cols.append(el.strip())
+        else:
+            cols.append(inp[st+1])
+    elif end-st>1:
+        for i in range(1,end-st):
+            if inp[st+i].strip()!=",":
+                cols.append(inp[st+i].strip().replace(",",""))
+    return cols
 def getCond(inp):
     if "where" in inp:
-        return inp.split("where")[1].strip()
+        w = inp.index("where")
+        if len(inp) - w == 1:
+            return inp[w+1]
+        else:
+            tmp = ""
+            for i in range(1,len(inp)-w):
+                tmp += inp[w+i].strip()
+            return tmp
     else:
         return ""
 
@@ -42,7 +52,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     break
                 data = data.decode("utf-8")
                 data = data.lower()
-                if data.startswith("select"):
+                data = data.split(" ")
+                for el in data:
+                    el = el.strip()
+                    if el == " ":
+                        data.remove(el)
+                if "select" not in data and "from" not in data:
+                    conn.sendall("Incorrect Input".encode())
+                elif data[0] == "select":
                     try:
                         table = getTable(data)
                         cols = getCols(data)
@@ -56,11 +73,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             db.select(table,cols[0])
                         elif cond != "" and cols[0]=="*":
                             db.select(table,cols[0],cond)
+                        elif cond == "" and cols[0]!="*" and len(cols)==1:
+                            db.select(table,cols)
+                        elif cond != "" and cols[0]!="*" and len(cols)==1:
+                            db.select(table,cols)
                         elif cond == "" and len(cols)>1:
                             db.select(table,cols)
                         elif cond != "" and len(cols)>1:
                             db.select(table,cols,cond)
                         result_string = result.getvalue()
                         conn.sendall(result_string.encode())
-                    except:
-                        conn.sendall("Input Error".encode())
+                    except Exception as e:
+                        conn.sendall(str(e).encode())
